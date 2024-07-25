fd34f98c5b2cd6fd6187c73c4fca61015a9f9c10
Thiago Silva <prof.thiagocontato@gmail.com>
2024-07-25 07:45:27 -0300

    revisando __super()__ function e aplicações desta.

--- POOpython/classes.py
+++ POOpython/classes.py
@@ -30,9 +30,9 @@ Aqui, faremos alguns exemplos de como classes filhas herdam métodos e proprieda
 A classe Estudante herdará todos os métodos e atributos da classe Pessoa.
 '''
  
-class Estudante(Pessoa): 
+'''class Estudante(Pessoa): 
     pass
-''' Se não quisermos adicionar mais métodos a classe Estudande, basta utilizarmos a palavra chave pass. Assim, qualquer objeto instanciado da classe Estudante terá somente os métodos definidos na classe pai, isto é, Pessoa.
+ Se não quisermos adicionar mais métodos a classe Estudande, basta utilizarmos a palavra chave 'pass'. Assim, qualquer objeto instanciado da classe Estudante terá somente os métodos definidos na classe pai, isto é, Pessoa.
 
 É importante notar que, se utilizarmos o método construtor na classe filha, o __init__() da classe filha sobrescreve o construtor da classe pai, ou seja, a classe filha não herda os atributos que foram construidos na classe pai, execeto se utilizarmos explicitamente:
 
@@ -43,7 +43,33 @@ class Filha(ClassePai):
 
 '''
 
-e1 = Estudante("Augustus", 2)
-print(e1) # Augustus, 2 anos.
-e1.falar() # Olá! Me chamo Augustus, tenho 2 anos.
 
+ # A função super()
+'''A função super() permite herdar todos os parâmetros/atributos da classe pai. 
+Usamos super__init__(atr1,...,atrn) para inicializar os atributos da super classe (classe pai, neste caso).
+
+O uso do segundo __init__(self,nome,idade, curso) acima de super() me permite definir novos atributos para classe Estudante e, utilizar os atributos da classe pai Pessoa por meio da inicialização feita abaixo com a função super()__init__().
+'''
+
+class Estudante(Pessoa):
+    def __init__(self, nome, idade, curso):
+        super().__init__(nome, idade)
+        self.curso = curso 
+   
+   # A representação legível do objeto instanciado desta classe, será a mesma que a da classe pai.
+    def __str__(self):
+        return super().__str__()
+    
+    def apresentacao(self):
+         print(f'Olá! Me chamo {self.nome}, tenho {self.idade} e curso {self.curso}.')
+
+#instanciando novos objetos    
+p = Pessoa('Mateus', 22)
+
+e = Estudante('Mateus', 22, 'Engenharia elátrica')
+
+# Chamando os métodos de cada classe.
+p.falar()
+
+e.apresentacao()
+print(e)
